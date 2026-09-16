905613994e00265700005f4589af75c45ca842b2
zie <zieezie23@gmail.com>
2026-09-16 11:22:00 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="pwa-hub-cloud-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="pwa-hub-cloud-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./assets/icon.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
