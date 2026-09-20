28c3f3505ad1b6bfa3c25c3672b07a0630b28eb7
zie <zieezie23@gmail.com>
2026-09-20 20:52:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C="pwa-launcher-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json","./assets/icon.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='check-item-tidak-bergerak-v20';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>c))));
